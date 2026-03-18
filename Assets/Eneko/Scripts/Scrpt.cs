98aa5766257789c96e44fd7bac4ad554bda8cdd9
Eneko123 <eneko_iglesias@euneiz.com>
2026-03-18 19:06:36 +0100

    Primeros pasos

---
+++ Assets/Eneko/Scripts/Scrpt.cs
@@ -0,0 +1,22 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Scrpt : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClicEnter()
+    {
+        Debug.Log("Click click click");
+    }
+}
